c36be18ea8b626fb9d4971c27176905421dc496c
MrPABITRAPRADHAN <gpabitrapadhan@gmail.com>
2025-04-14 23:23:18 +0530

    "diamond pattern"

---
+++ Pattern/diamond.java
@@ -0,0 +1,75 @@
+public class diamond {
+    public static void main(String[] args) {
+        
+          //? Diamond pattern
+        //       *
+        //     * * *
+        //   * * * * *
+        // * * * * * * *
+        //   * * * * * 
+        //     * * *
+        //       *
+        //  int n = 7;
+        //  int sp = n/2; 
+        //  int st =1;
+
+        //  for (int i = 1; i<=n; i++) {
+
+        //     for (int j = 1; j <= sp; j++) {
+
+        //         System.out.print("  ");
+                
+        //     }
+        //     for (int k = 1; k <= st; k++) {
+        //         // System.out.print(i+" ");
+        //         System.out.print(k+" ");
+        //         // System.out.print("*"+" ");                
+        //     }
+        //     if(i<n/2+1){
+        //         sp--;
+        //         st = st+2;
+        //     }else{
+        //         sp++;
+        //         st = st-2;
+        //     }
+        //     System.out.println();
+        // }
+
+        
+    //   1 2 3 4 5 6 7 
+    //     1 2 3 4 5
+    //       1 2 3
+    //         1
+    //       1 2 3
+    //     1 2 3 4 5
+    //   1 2 3 4 5 6 7    
+
+        //  int n = 7;
+        //  int sp = 0; 
+        //  int st =n;
+
+        //  for (int i = 1; i<=n; i++) {
+        //     for (int j = 1; j <= sp; j++) {
+        //         System.out.print("  ");
+                
+        //     }
+        //     for (int k = 1; k <= st; k++) {
+        //         // System.out.print(i+" ");
+        //         System.out.print(k+" ");
+        //         // System.out.print("*"+" ");                
+        //     }
+        //     if(i<n/2+1){
+        //         sp++;
+        //         st = st-2;
+        //     }else{
+        //         sp--;
+        //         st = st+2;
+        //     }
+        //     System.out.println();
+        // }
+
+        
+
+
+    }
+}
